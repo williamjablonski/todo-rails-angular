71e03349564f86e687f4ffbce7150829aff1ed66
Pedro <pedrohrs08@gmail.com>
2015-06-22 18:42:54 -0300

    adding user stories

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,4 +4,8 @@ class PagesController < ApplicationController
     
   end
 
+  def user_stories
+  	
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,9 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'pages#home'
 
+  root 'pages#home'
+  get 'user-stories' => 'pages#user_stories'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
