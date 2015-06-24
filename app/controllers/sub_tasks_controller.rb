cba89c3b88a335469fa3ef388e24966ac4ff8a1e
Pedro <pedrohrs08@gmail.com>
2015-06-24 15:20:43 -0300

    converting dates

--- app/controllers/sub_tasks_controller.rb
+++ app/controllers/sub_tasks_controller.rb
@@ -10,6 +10,7 @@ class SubTasksController < ApplicationController
 
     task = current_user.tasks.find(params[:sub_task][:task_id])
     if task.present?
+      sub_task_params[:due_date] = Date.new(sub_task_params[:due_date])
       sub_task = task.sub_tasks.create(sub_task_params)
       respond_with sub_task, location: [task, sub_task]
     end
